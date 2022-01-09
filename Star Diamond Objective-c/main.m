3d4d986768efa704b9979daaf43f6f5cd63b9012
Akram ELhayani <akramelhayani@gmail.com>
2022-01-09 23:23:33 +0200

    initial Commit

--- Star Diamond Objective-c/main.m
+++ Star Diamond Objective-c/main.m
@@ -8,8 +8,38 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
+    
+    int size = 50;
+    int mid =(size-(size%2))/2;// get the mid line
+    
+    
+    // print the first half
+    for (int count =1; count<=mid; count++) {
+        int skip = (mid+1) - count;
+        //char line[11] = " ";
+        char *line =malloc(size);
+        for (int i = 0 ; i<skip; i++) {
+            line[i] =' ';
+        }
+        for (int i = 0 ; i<count*2; i+=2) {
+            line[(skip + i)-1] =' ';
+            line[skip + i] ='*';
+        }
+        printf("%s\n", line);
     }
-    return NSApplicationMain(argc, argv);
+    
+   // print the secound half
+    for (int count =mid-1; count>0; count--) {
+        int skip = (mid+1) - count;
+        char *line =malloc(size);
+        for (int i = 0 ; i<skip; i++) {
+            line[i] =' ';
+        }
+        for (int i = 0 ; i<count*2; i+=2) {
+            line[(skip + i)-1] =' ';
+            line[skip + i] ='*';
+        }
+        printf("%s\n", line);
+    }
+    return 0;
 }
